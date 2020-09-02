7b768ed0030abf12e26b7e2e65785f6e5c1402b5
alffa <fabricio.alffa@gmail.com>
2020-09-02 10:18:57 -0600

    Creamos una funcion que donde enviamos por medio del request dos parametros, el nombre del usuario y la edad segun la documetacion de URL dispacher de Django

--- platzigram/platzigram/views.py
+++ platzigram/platzigram/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 #utilities
 from datetime import datetime
+import json 
 
 
 def hello_world(request):
@@ -13,7 +14,26 @@ def hello_world(request):
         now=datetime.now().strftime('%b %dth, %Y %H:%M hrs')
         ))
 
-def hi(request):
-    """hi"""
-    numbers = request.GET['numbers']
-    return HttpResponse(str(numbers))
+def sort_integers(request):
+    """return a JOSN response with sorted integers."""
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    sorted_ints = sorted(numbers)
+    #import pdb;pdb.set_trace()
+    data ={
+        'status': 'ok',
+        'numbers': sorted_ints,
+        'message': 'Integers sorted succesfully.'
+    }
+    return HttpResponse(
+        json.dumps(data,indent=4),
+        content_type='application/json'
+        )
+
+
+def say_hi(request,name,age):
+    """return a greeting"""
+    if age < 12:
+        message = 'Sorry {},you are not allowed here'.format(name)
+    else:
+        message = 'Hello, {}! Welcome to platzogram'.format(name)
+    return HttpResponse(message)
