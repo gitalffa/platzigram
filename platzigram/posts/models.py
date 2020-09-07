bc79a66db2f9253387c5b98a031c4cfaac438d92
alffa <fabricio.alffa@gmail.com>
2020-09-07 14:11:07 -0600

    creacion de la BD y algunos usuarios

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -13,6 +13,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    is_admin = models.TextField(default=False)
     bio = models.TextField(blank=True)
 
     birthdate = models.DateField(blank=True,null=True)
@@ -20,3 +21,6 @@ class User(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        """return email"""
+        return self.email
