724c27de93c1a8b4067498acc3c492eb72a6a8e0
alffa <fabricio.alffa@gmail.com>
2020-09-08 14:50:44 -0600

    creacion de profile con el admin de django

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -1,10 +1,16 @@
 """ urls.py """
+#Django
 from django.urls import path
+from django.contrib import admin
+
+
 from platzigram import views as local_views
 from posts import views as posts_views
 
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
+
     path('hello-world/',local_views.hello_world),
     path('sorted/',local_views.sort_integers),
     path('hi/<str:name>/<int:age>',local_views.say_hi),
