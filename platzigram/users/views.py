d6f28763976ee0945491adaf175350b440cafa02
alffa <fabricio.alffa@gmail.com>
2020-09-18 14:48:46 -0600

    se creo signup

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -2,8 +2,13 @@
 
 #django
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
+
+#models
+from django.contrib.auth.models import User
+from users.models import Profile
 
 def login_view(request):
     """login views"""
@@ -17,3 +22,31 @@ def login_view(request):
         else:
             return render(request,'users/login.html',{'error':'Invalid username and password'})
     return render(request,'users/login.html')
+
+@login_required
+def logout_view(request):
+    """logout a user"""
+    logout(request)
+    return redirect('login')
+
+
+def signup(request):
+    """signup view"""
+    if request.method == 'POST':
+        username=request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation=request.POST['passwd_confirmation']
+        if passwd != passwd_confirmation:
+            return render(request,'users/signup.html',{'error':'Password confirmation does not match'})
+
+        user = User.objects.create_user(username=username,password=passwd)
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+        return redirect('login')
+
+    return render(request,'users/signup.html')
