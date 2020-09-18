acf75797f2fefa741a78ead7543061bf319bb8a8
alffa <fabricio.alffa@gmail.com>
2020-09-18 15:05:16 -0600

    se creo signup y se integra un exception

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
 
+#exceptions
+from django.db.utils import IntegrityError
+
 #models
 from django.contrib.auth.models import User
 from users.models import Profile
@@ -38,8 +41,13 @@ def signup(request):
         passwd_confirmation=request.POST['passwd_confirmation']
         if passwd != passwd_confirmation:
             return render(request,'users/signup.html',{'error':'Password confirmation does not match'})
+        try:
+            user = User.objects.create_user(username=username,password=passwd)
+        except IntegrityError:
+            return render(request,'users/signup.html',{'error':'Username is already in use'})
 
-        user = User.objects.create_user(username=username,password=passwd)
+
+        
         user.first_name = request.POST['first_name']
         user.last_name = request.POST['last_name']
         user.email = request.POST['email']
